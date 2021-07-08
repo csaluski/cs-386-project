c9d32aee230df01ea1b2f9176d8e808bde1708f5
jadon <j@jadon.io>
2021-07-08 19:19:42 -0400

    Start web server using Vertx & Handlebars
    Signed-off-by: jadon <j@jadon.io>

---
+++ src/main/java/edu/nau/cs386/MainVerticle.java
@@ -0,0 +1,39 @@
+package edu.nau.cs386;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.common.template.TemplateEngine;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.TemplateHandler;
+import io.vertx.ext.web.templ.handlebars.HandlebarsTemplateEngine;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        // create the template engine
+        TemplateEngine engine = HandlebarsTemplateEngine.create(vertx);
+        TemplateHandler templateHandler = TemplateHandler.create(engine);
+
+        // create the http server and router
+        HttpServer server = vertx.createHttpServer();
+        Router router = Router.router(vertx);
+
+        // configure the router
+        router.route("/static/*").handler(StaticHandler.create("static"));
+        router.route("/").handler(templateHandler);
+
+        // start the http server
+        server.requestHandler(router)
+            .listen(8888, http -> {
+                if (http.succeeded()) {
+                    startPromise.complete();
+                    System.out.println("HTTP server started on port 8888");
+                } else {
+                    startPromise.fail(http.cause());
+                }
+            });
+    }
+}
